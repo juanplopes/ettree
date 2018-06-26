9dbf46895b292d12844256487479b07fc2a28bba
Juan Lopes <me@juanlopes.net>
2018-06-26 17:46:24 -0300

    test

---
+++ src/main/java/net/juanlopes/ettree/ETTree.java
@@ -0,0 +1,16 @@
+package net.juanlopes.ettree;
+
+public class ETTree<T extends Mergeable<T>> {
+    public int add(T obj) {
+        return 0;
+    }
+
+    public void link(int child, int parent) {
+
+    }
+
+    public void cut(int child) {
+
+    }
+
+}
