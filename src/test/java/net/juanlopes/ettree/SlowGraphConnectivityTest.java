1de155483e41f84f681cceafed571dfa687b98ec
Juan Lopes <me@juanlopes.net>
2018-10-18 10:16:14 -0300

    progress

--- src/test/java/net/juanlopes/ettree/SlowGraphConnectivityTest.java
+++ src/test/java/net/juanlopes/ettree/SlowGraphConnectivityTest.java
@@ -5,46 +5,67 @@ import org.junit.Test;
 
 import java.util.Arrays;
 import java.util.Random;
+import java.util.concurrent.CountDownLatch;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+import java.util.concurrent.TimeUnit;
 import java.util.stream.Collectors;
-import java.util.stream.IntStream;
 
 public class SlowGraphConnectivityTest {
     @Test
     @Ignore
     public void name() throws Exception {
-        int nodes = 1000, tests = 128, d = 12;
-
-        SlowGraphConnectivity[] G = IntStream.range(0, tests)
-                .mapToObj(x -> new SlowGraphConnectivity(nodes, d, x))
-                .toArray(SlowGraphConnectivity[]::new);
+        int nodes = 1000, tests = 2048, d = 12;
 
         int step = nodes / 100;
+        double R[][] = new double[100][d - 1];
+        ExecutorService executor = Executors.newFixedThreadPool(Runtime.getRuntime().availableProcessors());
 
-        for (int k = 0; k < 100; k++) {
-            double R[] = new double[d - 1];
+        CountDownLatch finished = new CountDownLatch(tests);
+        Random random = new Random();
 
-            int start = k * step;
-            int end = (k + 1) * step;
+        for (int test = 0; test < tests; test++) {
+            long seed = random.nextLong();
+            executor.submit(() -> {
+                SlowGraphConnectivity G = new SlowGraphConnectivity(nodes, d, seed);
+                for (int k = 0; k < 100; k++) {
+                    int start = k * step;
+                    int end = (k + 1) * step;
 
-            IntStream.range(0, tests).parallel().forEach(x -> {
-                for (int i = start; i < end; i++) {
-                    for (int j = i - 1; j >= 0 && j >= i - 32; j--)
-                        G[x].addEdge(i, j);
-                }
-                for (int i = 1; i < d; i++) {
-                    if (G[x].components(end, i) == 1) {
-                        synchronized (R) {
-                            R[i - 1]++;
+                    for (int i = start; i < end; i++) {
+                        for (int j = i - 1; j >= 0 && j >= i - 32; j--)
+                            G.addEdge(i, j);
+                    }
+                    for (int i = 1; i < d; i++) {
+                        if (G.components(end, i) == 1) {
+                            synchronized (R) {
+                                R[k][i - 1]++;
+                            }
                         }
                     }
                 }
+                finished.countDown();
             });
-            for (int i = 0; i < R.length; i++) {
-                R[i] /= tests;
+        }
+
+        long start = System.nanoTime();
+        long lastCount = tests;
+        while (!finished.await(1, TimeUnit.SECONDS)) {
+            long count = finished.getCount();
+            double rate = (System.nanoTime() - start) / (double) (tests - count) / 1e9;
+            if (lastCount != count)
+                System.out.println(finished.getCount() + "/" + tests + " (" + Math.round(count * rate) + ")");
+            lastCount = count;
+        }
+
+
+        for (int k = 0; k < 100; k++) {
+            for (int i = 0; i < R[k].length; i++) {
+                R[k][i] /= tests;
             }
 
-            String line = Arrays.stream(R).mapToObj(x -> "\t" + x).collect(Collectors.joining());
-            System.out.println(end + line);
+            String line = Arrays.stream(R[k]).mapToObj(x -> "\t" + x).collect(Collectors.joining());
+            System.out.println((k + 1) * step + line);
         }
 
     }
