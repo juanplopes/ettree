d6e1851e5cb7262333b1f73136b2dd2e9e79a0a9
Juan Lopes <me@juanlopes.net>
2018-11-17 13:52:13 -0200

    auto lib

--- build_lib.sh
+++ build_lib.sh
@@ -1,3 +1,4 @@
 #!/bin/bash
 cd src/main/cpp
-g++ -std=c++11 -shared -fPIC -O3 -I/usr/include -I"$JAVA_HOME/include" -I"$JAVA_HOME/include/linux" net_juanlopes_ettree_JNIWrapper.cpp -o libl0sampler.so
+mkdir -p ../resources
+g++ -std=c++11 -shared -fPIC -O3 -I/usr/include -I"$JAVA_HOME/include" -I"$JAVA_HOME/include/linux" net_juanlopes_ettree_JNIWrapper.cpp -o ../resources/libl0sampler.so
