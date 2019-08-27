e655c44de72fdbee377cd34dd919c7ddab3b957e
Juan Lopes <me@juanlopes.net>
2019-08-27 10:19:17 -0300

    find javahome

--- build_lib.sh
+++ build_lib.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
+JAVA_HOME=`type -p javac|xargs readlink -f|xargs dirname|xargs dirname`
 cd src/main/cpp
 mkdir -p ../resources
 g++ -std=c++11 -shared -fPIC -O3 -I/usr/include -I"$JAVA_HOME/include" -I"$JAVA_HOME/include/linux" net_juanlopes_ettree_JNIWrapper.cpp -o ../resources/libl0sampler.so
